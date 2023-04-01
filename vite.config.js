8981852591435c688ba81434ad8451846fca4442
alfa <128838529+saririuvu@users.noreply.github.com>
2023-04-01 09:17:55 +0000

    update

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { imagetools } from 'vite-imagetools';
+import { thumbHash } from 'vite-plugin-thumbhash';
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	define: {
 		'process.env.VITE_BUILD_TIME': JSON.stringify(new Date().toISOString()),
 	},
-	plugins: [imagetools(), sveltekit()],
+	plugins: [imagetools(), sveltekit(), thumbHash()],
 	server: {
 		port: 5173,
 		strictPort: false,
